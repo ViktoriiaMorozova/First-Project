70614a05f0998914fbdb82b19f3a4a74ed046416
Edward Matsyshen <edward.matsyshen@bellevuecollege.edu>
2023-09-20 19:17:13 -0700

    added ed page

--- firstfile.js
+++ firstfile.js
@@ -1,6 +1,14 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Handle incoming requests here
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
